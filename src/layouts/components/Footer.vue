3b437f7ec45d9564e37428d67d432b62159f43c0
ahmdkenz <gigibengal14@gmail.com>
2026-05-29 10:40:58 +0700

    Update Footer

--- src/layouts/components/Footer.vue
+++ src/layouts/components/Footer.vue
@@ -1,41 +1,7 @@
 <template>
-  <div class="h-100 d-flex align-center justify-space-between text-medium-emphasis">
-    <!-- 👉 Footer: left content -->
+  <div class="h-100 d-flex align-center justify-center text-medium-emphasis">
     <div class="d-flex align-center text-base">
-      &copy;
-      {{ new Date().getFullYear() }},
-      Made With
-      <VIcon
-        icon="ri-heart-fill"
-        color="error"
-        size="1.25rem"
-        class="mx-1"
-      />
-      By <a
-        href="https://pixinvent.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="text-primary ms-1"
-      >Pixinvent</a>
+      &copy; 2026 Intelligent of Record Organization Number by Sheza Mitra Amanah &nbsp;|&nbsp; v1.0.0
     </div>
-    <!-- 👉 Footer: right content -->
-    <span class="d-md-flex gap-x-4 text-primary d-none">
-      <a
-        href="https://themeforest.net/licenses/standard"
-        target="noopener noreferrer"
-      >License</a>
-      <a
-        href="https://1.envato.market/pixinvent_portfolio"
-        target="noopener noreferrer"
-      >More Themes</a>
-      <a
-        href="https://demos.pixinvent.com/materialize-vuejs-admin-template/documentation/"
-        target="noopener noreferrer"
-      >Documentation</a>
-      <a
-        href="https://pixinvent.ticksy.com/"
-        target="noopener noreferrer"
-      >Support</a>
-    </span>
   </div>
 </template>
