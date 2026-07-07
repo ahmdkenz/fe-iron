397fa7c4eeddd7de834b0d4a069e0801d2ec1a91
ahmdkenz <gigibengal14@gmail.com>
2026-07-07 09:57:17 +0700

    Update Mobile Version

--- src/layouts/components/Footer.vue
+++ src/layouts/components/Footer.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="h-100 d-flex align-center justify-center text-medium-emphasis">
-    <div class="d-flex align-center text-base">
+  <div class="h-100 d-flex align-center justify-center text-medium-emphasis footer-wrapper">
+    <div class="d-flex align-center text-base footer-text">
       &copy; 2026 Intelligent of Record Organization Number by Sheza Mitra Amanah &nbsp;|&nbsp; v1.0.0
     </div>
   </div>
 </template>
+
+<style scoped>
+@media (max-width: 600px) {
+  .footer-wrapper {
+    padding-inline: 8px;
+  }
+
+  .footer-text {
+    font-size: 0.65rem;
+    line-height: 1.1;
+    text-align: center;
+    white-space: normal;
+  }
+}
+</style>
